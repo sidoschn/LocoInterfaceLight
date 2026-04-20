b1ccbbed5f1b99f9a17e12d58a01ab65fd75d34e
sidoschn <dominik.schneidereit@fau.de>
2026-04-20 15:03:12 +0200

    everything still buggy and not running

--- LocoInterfaceLight/tcpServer.cpp
+++ LocoInterfaceLight/tcpServer.cpp
@@ -1,24 +1,42 @@
 #include <tcpServer.h>
-namespace http
-{
-    TcpServer::TcpServer()
-    {
+#include <exception>
+#include <vector>
+#include <iostream>
+#include <Bits.h>
+#include <sstream>
+#include <stdlib.h>
+#include <io.h>
+//: 
+
+http::TcpServer::TcpServer(std::string ip_address, int port) {
+        : m_ip_address(ip_address), m_port(port), m_socket(), m_new_socket(), m_incomingMessage(), m_socketAddress(), m_socketAddress_len(sizeof(m_socketAddress)), m_serverMessage(buildResponse())
+    
         startServer();
     }
         
     
     
-    TcpServer::~TcpServer()
+    http::TcpServer::~TcpServer()
     {
         closeServer();
     }
 
-    int TcpServer::startServer() {
+    int http::TcpServer::startServer() {
+
+        if (WSAStartup(MAKEWORD(2, 0), &m_wsaData) != 0)
+        {
+            exitWithError("WSAStartup failed");
+        }
+        http::m_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (m_socket < 0)
+        {
+            exitWithError("Cannot create socket");
+            return 1;
+        }
+        return 0;
+    }
+
+    void http::TcpServer::closeServer() {
 
     }
 
-    void TcpServer::closeServer() {
-
-    }
-
-} // namespace http
--- LocoInterfaceLight/tcpServer.h
+++ LocoInterfaceLight/tcpServer.h
@@ -1,6 +1,8 @@
 //#pragma once
+
 #ifndef INCLUDED_TCPSERVER
 #define INCLUDED_TCPSERVER
+#include <winsock.h>
 
 namespace http
 {
@@ -10,6 +12,14 @@ namespace http
 		~TcpServer();
 
 	private:
+		std::string m_ip_address;
+		int m_port;
+		int m_socket;
+		int m_new_socket;
+		long m_incomingMessage;
+		struct sockaddr_in m_socketAddress;
+		unsigned int m_socketAddress_len;
+		std::string m_serverMessage;
 
 		int startServer();
 		void closeServer();
