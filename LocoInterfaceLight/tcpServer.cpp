890f76ada6058158f7fd4f517b9fc81e5197409c
sidoschn <dominik.schneidereit@fau.de>
2026-04-20 15:20:52 +0200

    added httplib.h

--- LocoInterfaceLight/tcpServer.cpp
+++ LocoInterfaceLight/tcpServer.cpp
@@ -6,10 +6,11 @@
 #include <sstream>
 #include <stdlib.h>
 #include <io.h>
+#include <Windows.h>
+#include <WS2tcpip.h>
 //: 
 
-http::TcpServer::TcpServer(std::string ip_address, int port) {
-        : m_ip_address(ip_address), m_port(port), m_socket(), m_new_socket(), m_incomingMessage(), m_socketAddress(), m_socketAddress_len(sizeof(m_socketAddress)), m_serverMessage(buildResponse())
+http::TcpServer::TcpServer() {
     
         startServer();
     }
@@ -22,12 +23,13 @@ http::TcpServer::TcpServer(std::string ip_address, int port) {
     }
 
     int http::TcpServer::startServer() {
+        WSADATA wsaData;
 
-        if (WSAStartup(MAKEWORD(2, 0), &m_wsaData) != 0)
+        if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
         {
             exitWithError("WSAStartup failed");
         }
-        http::m_socket = socket(AF_INET, SOCK_STREAM, 0);
+        m_socket = socket(AF_INET, SOCK_STREAM, 0);
         if (m_socket < 0)
         {
             exitWithError("Cannot create socket");
